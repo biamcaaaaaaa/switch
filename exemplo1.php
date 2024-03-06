b205f8cd0e7fbed03d95d5bcea5e674ecf05d412
biamcaaaaaaa <biancacgiroto@gmail.com>
2024-03-06 16:19:38 -0300

    primeiro commit
    atividade de github

---
+++ exemplo1.php
@@ -0,0 +1,12 @@
+<?php
+$i = 3;
+switch ($i) {
+    case 0:
+    case 1:
+    case 2:
+        echo "i é menor que 3 mas não é negativo";
+        break;
+    case 3:
+        echo "i é 3";
+}
+?>
---
+++ exemplo3.php
@@ -0,0 +1,15 @@
+<?php
+$cerveja = "amstel";
+switch($cerveja)
+{
+    case 'amstel';
+    case 'brahma';
+    case 'stella';
+    case 'heineken';
+        echo 'Boa escolha';
+        break;
+    default;
+        echo 'Por favor faça uma nova seleção...';
+        break;
+}
+?>
